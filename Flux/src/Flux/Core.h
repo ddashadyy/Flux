aa1d0de58524f697d3c4f0a283dc5f0df39cbee8
ddashdyy <poletaevgena75@gmail.com>
2026-03-09 16:11:07 +0800

    assertions

--- Flux/src/Flux/Core.h
+++ Flux/src/Flux/Core.h
@@ -11,4 +11,15 @@
 	#error Flux only supports Windows!
 #endif 
 
+#ifdef FL_ENABLE_ASSERTS
+	#define FL_ASSERT(x, ...) { if(!(x)) { FL_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define FL_CORE_ASSERT(x, ...) { if(!(x)) { FL_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define FL_ASSERT(x, ...)
+	#define FL_CORE_ASSERT(x, ...)
+#endif
+
+
+
 #define BIT(x) (1 << x)
+
