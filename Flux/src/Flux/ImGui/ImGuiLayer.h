1d618deaa98f9a7b05396f2ead0b0fe8a86b0293
ddashdyy <poletaevgena75@gmail.com>
2026-03-11 20:09:39 +0800

    ImGui layer

--- Flux/src/Flux/ImGui/ImGuiLayer.h
+++ Flux/src/Flux/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Flux/Layer.h"
 
+#include "Flux/Events/ApplicationEvent.h"
+#include "Flux/Events/MouseEvent.h"
+#include "Flux/Events/KeyEvent.h"
+
 namespace Flux {
 
 	class FLUX_API ImGuiLayer : public Layer
@@ -14,6 +18,17 @@ namespace Flux {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 	};
