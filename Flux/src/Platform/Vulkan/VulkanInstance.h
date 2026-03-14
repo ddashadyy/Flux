068e46dc55f25f52fa1b149ad02f67e3cd88de29
ddashdyy <poletaevgena75@gmail.com>
2026-03-14 12:08:43 +0800

    Vulkan instance with vk-bootstrap

---
+++ Flux/src/Platform/Vulkan/VulkanInstance.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <VkBootstrap.h>
+
+#include "Flux/Core.h"
+
+namespace Flux {
+
+	class VulkanInstance
+	{
+	public:
+		FL_NON_COPYABLE(VulkanInstance);
+
+		VulkanInstance() = default;
+		~VulkanInstance();
+
+		void Init(const char* appName = "Flux");
+
+		inline VkInstance       GetInstance() const { return m_Instance.instance; }
+		inline VkSurfaceKHR     GetSurface()  const { return m_Surface; }
+		inline vkb::Instance&   GetVkbInstance() { return m_Instance; }
+
+		void CreateSurface(GLFWwindow* window);
+
+	private:
+		vkb::Instance m_Instance;
+		VkSurfaceKHR  m_Surface = VK_NULL_HANDLE;
+	};
+
+}
