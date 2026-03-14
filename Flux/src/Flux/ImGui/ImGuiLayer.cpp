02f95be14954b623384561d4df22e4796307aa66
ddashdyy <poletaevgena75@gmail.com>
2026-03-14 11:59:21 +0800

    Impl for Vulkan graphics backend

--- Flux/src/Flux/ImGui/ImGuiLayer.cpp
+++ Flux/src/Flux/ImGui/ImGuiLayer.cpp
@@ -2,10 +2,13 @@
 #include "ImGuiLayer.h"
 
 #include <imgui.h>
+
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_vulkan.h>
 
 #include "Flux/Application.h"
+#include "Platform/Windows/WindowsWindow.h"
+#include "Platform/Vulkan/VulkanContext.h"
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -32,26 +35,77 @@ namespace Flux {
 		ImGui::StyleColorsDark();
 
 		Application& app = Application::Get();
-		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
-		ImGui_ImplGlfw_InitForVulkan(window, true);
+		auto& window = app.GetWindow();
+
+		GLFWwindow* glfwWindow = static_cast<GLFWwindow*>(window.GetNativeWindow());
+
+		VulkanContext& context = static_cast<VulkanContext&>(
+			static_cast<WindowsWindow&>(window).GetContext()
+			);
+
+		ImGui_ImplGlfw_InitForVulkan(glfwWindow, true);
+
+		ImGui_ImplVulkan_InitInfo initInfo{};
+		initInfo.ApiVersion = VK_API_VERSION_1_2;
+		initInfo.Instance = context.GetInstance();
+		initInfo.PhysicalDevice = context.GetPhysicalDevice();
+		initInfo.Device = context.GetDevice();
+		initInfo.QueueFamily = context.GetGraphicsQueueFamily();
+		initInfo.Queue = context.GetGraphicsQueue();
+		initInfo.DescriptorPool = context.GetDescriptorPool();
+		initInfo.MinImageCount = context.GetMinImageCount();
+		initInfo.ImageCount = context.GetImageCount();
+		initInfo.PipelineInfoMain.RenderPass = context.GetRenderPass();
+
+		ImGui_ImplVulkan_Init(&initInfo);
+
+		FL_CORE_INFO("ImGui Vulkan backend initialized");
 	}
 
 	void ImGuiLayer::OnDetach()
 	{
+		Application& app = Application::Get();
+		VulkanContext& context = static_cast<VulkanContext&>(
+			static_cast<WindowsWindow&>(app.GetWindow()).GetContext()
+			);
+
+		vkDeviceWaitIdle(context.GetDevice());
+
+		ImGui_ImplVulkan_Shutdown();
 		ImGui_ImplGlfw_Shutdown();
 		ImGui::DestroyContext();
 	}
 
 	void ImGuiLayer::OnUpdate()
 	{
+		ImGui_ImplVulkan_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
 		static bool show = true;
 		ImGui::ShowDemoWindow(&show);
 
+		// Своё окно
+		ImGui::Begin("Flux Engine");
+		ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+		ImGui::Separator();
+
+		static float clearColor[3] = { 0.1f, 0.1f, 0.1f };
+		ImGui::ColorEdit3("Clear Color", clearColor);
+
+		if (ImGui::Button("Click me!"))
+			FL_CORE_INFO("Button clicked!");
+
+		ImGui::End();
+
 		ImGui::Render();
-		// TODO: ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), commandBuffer);
+
+		Application& app = Application::Get();
+		VulkanContext& context = static_cast<VulkanContext&>(
+			static_cast<WindowsWindow&>(app.GetWindow()).GetContext()
+		);
+
+		ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), context.GetCurrentCommandBuffer());
 	}
 
 	void ImGuiLayer::OnEvent(Event& event)
