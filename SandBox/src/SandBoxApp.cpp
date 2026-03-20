e91851d19e0963f276f93d3637b3327452764194
ddashdyy <poletaevgena75@gmail.com>
2026-03-20 20:01:49 +0800

    First triangle

--- SandBox/src/SandBoxApp.cpp
+++ SandBox/src/SandBoxApp.cpp
@@ -2,31 +2,38 @@
 
 #include "imgui/imgui.h"
 
+
 class ExampleLayer : public Flux::Layer
 {
 public:
 	ExampleLayer()
 		: Layer("Example")
 	{
+		FL_INFO("Creating shader...");
+		m_Shader = Flux::Shader::Create("C:/dev/Flux/Sandbox/assets/shaders/shader");
+		FL_INFO("Creating pipeline...");
+		m_Pipeline = Flux::Pipeline::Create(m_Shader);
+		FL_INFO("ExampleLayer ready");
 	}
 
 	void OnUpdate() override
 	{
-		//FL_INFO("ExampleLayer::Update");
-		if (Flux::Input::IsKeyPressed(FL_KEY_TAB))
-			FL_TRACE("Tab key is pressed");
+		auto& context = Flux::VulkanContext::Get();
+		VkCommandBuffer cmd = context.GetCurrentCommandBuffer();
+
+		m_Pipeline->Bind();
+		vkCmdDraw(cmd, 3, 1, 0, 0);
 	}
 
 	virtual void OnImGuiRender() override
 	{
-		ImGui::Begin("Test");
-		ImGui::Text("Hello World");
-		ImGui::End();
+		//ImGui::Begin("Test");
+		//ImGui::Text("Hello World");
+		//ImGui::End();
 	}
 
 	void OnEvent(Flux::Event& event) override
 	{
-
 		if (event.GetEventType() == Flux::EventType::KeyPressed)
 		{
 			Flux::KeyPressedEvent& e = static_cast<Flux::KeyPressedEvent&>(event);
@@ -34,6 +41,9 @@ public:
 		}
 	}
 
+private: 
+	Flux::Ref<Flux::Shader>   m_Shader;
+	Flux::Ref<Flux::Pipeline> m_Pipeline;
 };
 
 class Sandbox : public Flux::Application
@@ -41,8 +51,7 @@ class Sandbox : public Flux::Application
 public:
 	Sandbox()
 	{
-		//PushLayer(new ExampleLayer());
-		//PushOverlay(new Flux::ImGuiLayer());
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
