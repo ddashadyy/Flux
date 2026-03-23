25d6a817660e586602ed7cfef320a78d97beb686
ddashdyy <poletaevgena75@gmail.com>
2026-03-23 19:11:12 +0800

    Now pipeline works with UBO

--- Flux/src/Platform/Vulkan/VulkanPipeline.h
+++ Flux/src/Platform/Vulkan/VulkanPipeline.h
@@ -16,15 +16,20 @@ namespace Flux {
         FL_NON_COPYABLE(VulkanPipeline);
 
         void Bind() const override;
+        void SetUniformBuffer(const Ref<UniformBuffer>& uniformBuffer) override;
 
         inline VkPipeline GetPipeline() const { return m_Pipeline; }
 
     private:
         void CreatePipelineLayout();
         void CreatePipeline(const Ref<Shader>& shader);
+        void CreateDescriptorSet(VkBuffer uniformBuffer);
 
     private:
-        VkPipeline       m_Pipeline       = VK_NULL_HANDLE;
-        VkPipelineLayout m_PipelineLayout = VK_NULL_HANDLE;
+        VkPipeline            m_Pipeline            = VK_NULL_HANDLE;
+        VkPipelineLayout      m_PipelineLayout      = VK_NULL_HANDLE;
+
+        VkDescriptorSetLayout m_DescriptorSetLayout = VK_NULL_HANDLE;
+        VkDescriptorSet       m_DescriptorSet       = VK_NULL_HANDLE;
 	};
 }
