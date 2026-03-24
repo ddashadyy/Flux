6087e42933c65d1edb878bf624200c8ea5ad8808
ddashdyy <poletaevgena75@gmail.com>
2026-03-24 12:40:55 +0800

    Now pipeline takes binding and attribute descriptions from buffer layouts

--- Flux/src/Flux/Geometry/Vertex.h
+++ Flux/src/Flux/Geometry/Vertex.h
@@ -1,11 +1,6 @@
 #pragma once
 
-
 #include <glm/glm.hpp>
-#include <vulkan/vulkan.h>
-
-#include <array>
-#include <cstddef>
 
 namespace Flux {
 
@@ -13,32 +8,5 @@ namespace Flux {
 	{
 		glm::vec3 Position;
 		glm::vec3 Color;
-
-		static VkVertexInputBindingDescription getBindingDescription()
-		{
-			VkVertexInputBindingDescription bindingDescription{};
-			bindingDescription.binding = 0;
-			bindingDescription.stride = sizeof(Vertex);
-			bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-			return bindingDescription;
-		}
-
-		static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions()
-		{
-			std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
-
-			attributeDescriptions[0].binding = 0;
-			attributeDescriptions[0].location = 0;
-			attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-			attributeDescriptions[0].offset = offsetof(Vertex, Position);
-
-			attributeDescriptions[1].binding = 0;
-			attributeDescriptions[1].location = 1;
-			attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-			attributeDescriptions[1].offset = offsetof(Vertex, Color);
-
-			return attributeDescriptions;
-		}
 	};
 }
