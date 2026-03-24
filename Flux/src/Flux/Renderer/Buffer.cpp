4560a8ef0c287a4d5ad7a087019bfa326e6e54af
ddashdyy <poletaevgena75@gmail.com>
2026-03-24 12:42:04 +0800

    Added default return

--- Flux/src/Flux/Renderer/Buffer.cpp
+++ Flux/src/Flux/Renderer/Buffer.cpp
@@ -12,6 +12,9 @@ namespace Flux {
 		case RendererAPI::API::Vulkan: return Flux::CreateScope<VulkanVertexBuffer>(size);
 		case RendererAPI::API::None:   FL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 		}
+
+		FL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+		return nullptr;
 	}
 
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
@@ -21,23 +24,20 @@ namespace Flux {
 		case RendererAPI::API::Vulkan: return Flux::CreateScope<VulkanVertexBuffer>(vertices, size);
 		case RendererAPI::API::None:   FL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 		}
-	}
-	
-	Ref<IndexBuffer> IndexBuffer::Create(uint32_t size)
-	{
-		switch (RendererAPI::GetAPI())
-		{
-		case RendererAPI::API::Vulkan: return Flux::CreateScope<VulkanIndexBuffer>(size);
-		case RendererAPI::API::None:   FL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		}
+
+		FL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+		return nullptr;
 	}
 
-	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t count)
 	{
 		switch (RendererAPI::GetAPI())
 		{
-		case RendererAPI::API::Vulkan: return Flux::CreateScope<VulkanIndexBuffer>(indices, size);
+		case RendererAPI::API::Vulkan: return Flux::CreateScope<VulkanIndexBuffer>(indices, count);
 		case RendererAPI::API::None:   FL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 		}
+
+		FL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+		return nullptr;
 	}
 }
