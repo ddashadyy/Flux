596b4d1997349fc50a7315b717fcc36de020bcc6
ddashdyy <poletaevgena75@gmail.com>
2026-03-28 00:18:02 +0800

    Added method to work with graphics API

--- Flux/src/Flux/Renderer/RendererAPI.h
+++ Flux/src/Flux/Renderer/RendererAPI.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Flux/Core.h"
+#include <glm/glm.hpp>
+
 namespace Flux {
 
     class RendererAPI
@@ -11,7 +14,17 @@ namespace Flux {
             Vulkan = 1
         };
 
-        static API GetAPI() { return s_API; }
+    public:
+        virtual ~RendererAPI() = default;
+
+        virtual void SetViewport(float width, float height, float x = 0.0f, float y = 0.0f, float minDepth = 0.0f, float maxDepth = 1.0f) = 0;
+
+        virtual void BeginRenderPass(const glm::vec4& color) = 0;
+        virtual void EndRenderPass() = 0;
+
+		virtual void DrawIndexed(uint32_t indexCount = 0) = 0;
+
+        inline static API GetAPI() { return s_API; }
 
         static const char* GetAPIName()
         {
@@ -22,6 +35,8 @@ namespace Flux {
             }
             return "Unknown";
         }
+
+		static Scope<RendererAPI> Create();
 
     private:
         static API s_API;
