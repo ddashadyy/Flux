46fd7367dd500a77535f6022df3c034a67dd8ebf
ddashdyy <poletaevgena75@gmail.com>
2026-04-09 14:00:36 +0800

    Updated to new RHI

--- Flux/src/Flux/Application.cpp
+++ Flux/src/Flux/Application.cpp
@@ -1,116 +1,116 @@
 #include "flpch.h"
 #include "Application.h"
-
 #include "Flux/Log.h"
 #include "Flux/Core.h"
-
-#include "Renderer/Renderer.h"
-#include "Renderer/RenderCommand.h"
-
-#include <GLFW/glfw3.h>
-
 #include "Input.h"
+#include <GLFW/glfw3.h>
 
 namespace Flux {
 
-	Application* Application::s_Instance = nullptr;
+    Application* Application::s_Instance = nullptr;
 
-	Application::Application()
-	{
-		FL_CORE_ASSERT(!s_Instance, "Application already exists!");
-		s_Instance = this;
+    Application::Application()
+    {
+        FL_CORE_ASSERT(!s_Instance, "Application already exists!");
+        s_Instance = this;
 
-		m_Window = std::unique_ptr<Window>(Window::Create());
-		m_Window->SetEventCallback(FL_BIND_EVENT_FN(Application::OnEvent));
+        m_Window = Scope<Window>(Window::Create());
+        m_Window->SetEventCallback(FL_BIND_EVENT_FN(Application::OnEvent));
 
-		m_ImGuiLayer = new ImGuiLayer();
-		PushOverlay(m_ImGuiLayer);
-	}
+        void* nativeWindow = m_Window->GetNativeWindow();
+        uint32_t width = m_Window->GetWidth();
+        uint32_t height = m_Window->GetHeight();
 
-	Application::~Application()
-	{
-		PopOverlay(m_ImGuiLayer);
-		delete m_ImGuiLayer;
-	}
+        m_Device = RHIFactory::CreateDevice(RendererBackend::Auto, nativeWindow, width, height);
 
-	void Application::PushLayer(Layer* layer)
-	{
-		m_LayerStack.PushLayer(layer);
-	}
+        m_FrameFences.resize(MAX_FRAMES_IN_FLIGHT);
+        m_ImageAvailable.resize(MAX_FRAMES_IN_FLIGHT);
+        m_RenderFinished.resize(MAX_FRAMES_IN_FLIGHT);
 
-	void Application::PushOverlay(Layer* layer)
-	{
-		m_LayerStack.PushOverlay(layer);
-	}
+        for (uint32_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) 
+        {
+            m_FrameFences[i] = m_Device->CreateFence(true);
+            m_ImageAvailable[i] = m_Device->CreateSemaphore();
+            m_RenderFinished[i] = m_Device->CreateSemaphore();
+        }
 
-	void Application::PopLayer(Layer* layer)
-	{
-		m_LayerStack.PopLayer(layer);
-	}
+        m_ImGuiLayer = new ImGuiLayer();
+        PushOverlay(m_ImGuiLayer);
+    }
 
-	void Application::PopOverlay(Layer* layer)
-	{
-		m_LayerStack.PopOverlay(layer);
-	}
+    Application::~Application()
+    {
+        PopOverlay(m_ImGuiLayer);
+        delete m_ImGuiLayer;
+    }
 
-	void Application::OnEvent(Event& e)
-	{
-		EventDispatcher dispatcher(e);
-		dispatcher.Dispatch<WindowCloseEvent>(FL_BIND_EVENT_FN(Application::OnWindowClose));
-		dispatcher.Dispatch<WindowResizeEvent>(FL_BIND_EVENT_FN(Application::OnWindowResize));
-		//dispatcher.Dispatch<AppRenderEvent>(FL_BIND_EVENT_FN(Application::OnAppRender));
-	
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
-		{
-			(*--it)->OnEvent(e);
-			if (e.Handled) break;
-		}
-	}
+    void Application::Run()
+    {
+        while (m_Running) {
+            RenderFrame();
+            m_Window->OnUpdate();
+        }
+    }
 
-	void Application::Run()
-	{
-		while (m_Running)
-		{
-			RenderFrame();
-			m_Window->OnUpdate();
-		}
-	}
+    void Application::OnEvent(Event& e)
+    {
+        EventDispatcher dispatcher(e);
+        dispatcher.Dispatch<WindowCloseEvent>(FL_BIND_EVENT_FN(Application::OnWindowClose));
+        dispatcher.Dispatch<WindowResizeEvent>(FL_BIND_EVENT_FN(Application::OnWindowResize));
 
-	bool Application::OnWindowClose(WindowCloseEvent& e)
-	{
-		m_Running = false;
-		return true;
-	}
+        for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();) {
+            (*--it)->OnEvent(e);
+            if (e.Handled) break;
+        }
+    }
 
-	bool Application::OnWindowResize(WindowResizeEvent& e)
-	{
-		//Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
-		return false;
-	}
+    void Application::PushLayer(Layer* layer) { m_LayerStack.PushLayer(layer); }
+    void Application::PushOverlay(Layer* layer) { m_LayerStack.PushOverlay(layer); }
+    void Application::PopLayer(Layer* layer) { m_LayerStack.PopLayer(layer); }
+    void Application::PopOverlay(Layer* layer) { m_LayerStack.PopOverlay(layer); }
 
-	bool Application::OnAppRender(AppRenderEvent& e)
-	{
-		return false;
-	}
+    bool Application::OnWindowClose(WindowCloseEvent& e)
+    {
+        m_Running = false;
+        return true;
+    }
 
-	void Application::RenderFrame()
-	{
-		m_Window->GetContext().BeginFrame();
+    bool Application::OnWindowResize(WindowResizeEvent& e)
+    {
+        m_Device->GetSwapchain()->Resize(e.GetWidth(), e.GetHeight());
+        return false;
+    }
 
-		RenderCommand::SetViewport(m_Window->GetWidth(), m_Window->GetHeight(), 0, 0, 0, 1);
-		RenderCommand::BeginRenderPass({ 0.1f, 0.1f, 0.1f, 1.0f });
+    void Application::RenderFrame()
+    {
+        auto* swapchain = m_Device->GetSwapchain();
+        auto* cmdList = m_Device->GetCommandList();
 
-		for (Layer* layer : m_LayerStack)
-			layer->OnUpdate();
+        m_FrameFences[m_CurrentFrame]->Wait();
+        m_FrameFences[m_CurrentFrame]->Reset();
 
-		m_ImGuiLayer->Begin();
-		for (Layer* layer : m_LayerStack)
-			layer->OnImGuiRender();
-		m_ImGuiLayer->End();
+        swapchain->AcquireNextImage(m_ImageAvailable[m_CurrentFrame].get());
 
-		RenderCommand::EndRenderPass();
+        cmdList->Begin();
 
-		m_Window->GetContext().EndFrame();
-	}
+        for (Layer* layer : m_LayerStack)
+            layer->OnUpdate();
+
+        m_ImGuiLayer->Begin();
+        for (Layer* layer : m_LayerStack)
+            layer->OnImGuiRender();
+        m_ImGuiLayer->End();
+
+        cmdList->End();
+        cmdList->Submit(
+            m_FrameFences[m_CurrentFrame].get(),
+            m_ImageAvailable[m_CurrentFrame].get(),
+            m_RenderFinished[m_CurrentFrame].get()
+        );
+
+        swapchain->Present(m_RenderFinished[m_CurrentFrame].get());
+
+        m_CurrentFrame = (m_CurrentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
+    }
 
 }
--- Flux/src/Flux/Application.h
+++ Flux/src/Flux/Application.h
@@ -4,49 +4,60 @@
 
 #include "Flux/Events/Event.h"
 #include "Flux/Events/ApplicationEvent.h"
-#include "Flux/LayerStack.h"
 
+#include "Flux/LayerStack.h"
 #include "Flux/ImGui/ImGuiLayer.h"
+
+#include "Flux/Renderer/RHIDevice.h"
+#include "Flux/Renderer/RHIFactory.h"
+#include "Flux/Renderer/RHISwapchain.h"
 
 namespace Flux {
 
-	class FLUX_API Application
-	{
-	public:
-		Application();
-		virtual ~Application();
+    class FLUX_API Application
+    {
+    public:
+        Application();
+        virtual ~Application();
 
-		void Run();
+        void Run();
+        void OnEvent(Event& e);
 
-		void OnEvent(Event& e);
+        void PushLayer(Layer* layer);
+        void PushOverlay(Layer* layer);
+        void PopLayer(Layer* layer);
+        void PopOverlay(Layer* layer);
 
-		void PushLayer(Layer* layer);
-		void PushOverlay(Layer* layer);
+        inline Window& GetWindow() { return *m_Window; }
+        inline RHIDevice& GetDevice() { return *m_Device; }
+        inline RHISwapchain& GetSwapchain() { return *m_Device->GetSwapchain(); }
 
-		void PopLayer(Layer* layer);
-		void PopOverlay(Layer* layer);
+        inline static Application& Get() { return *s_Instance; }
 
-		inline Window& GetWindow() { return *m_Window; }
-		inline static Application& Get() { return *s_Instance; }
+    private:
+        bool OnWindowClose(WindowCloseEvent& e);
+        bool OnWindowResize(WindowResizeEvent& e);
+        void RenderFrame();
 
-	private:
-		bool OnWindowClose(WindowCloseEvent& e);
-		bool OnWindowResize(WindowResizeEvent& e);
-		bool OnAppRender(AppRenderEvent& e);
+    private:
+        Scope<Window>     m_Window;
+        Scope<RHIDevice>  m_Device;
 
-		void RenderFrame();
+        std::vector<Scope<RHIFence>>     m_FrameFences;
+        std::vector<Scope<RHISemaphore>> m_ImageAvailable;
+        std::vector<Scope<RHISemaphore>> m_RenderFinished;
 
-	private:
-		Scope<Window> m_Window;
-		ImGuiLayer* m_ImGuiLayer = nullptr;
+        uint32_t m_CurrentFrame = 0;
+        static constexpr uint32_t MAX_FRAMES_IN_FLIGHT = 2;
 
-		LayerStack m_LayerStack;
-		bool m_Running = true;
+        ImGuiLayer* m_ImGuiLayer = nullptr;
+        LayerStack  m_LayerStack;
+        bool        m_Running = true;
 
-	private:
-		static Application* s_Instance;
-	};
+    private:
+        static Application* s_Instance;
+    };
 
-	// To be defined in CLIENT
-	Application* CreateApplication();
+    Application* CreateApplication();
+
 }
