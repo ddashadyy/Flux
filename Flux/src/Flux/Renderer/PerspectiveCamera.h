b219ec7a4356835427c17ad0d625c5ce133f10e7
ddashdyy <poletaevgena75@gmail.com>
2026-04-09 14:01:13 +0800

    Make inline getters

--- Flux/src/Flux/Renderer/PerspectiveCamera.h
+++ Flux/src/Flux/Renderer/PerspectiveCamera.h
@@ -14,8 +14,8 @@ namespace Flux {
         void SetPosition(const glm::vec3& position);
         void SetRotation(const glm::vec3& rotation); // pitch, yaw, roll в градусах
 
-        const glm::vec3& GetPosition() const { return m_Position; }
-        const glm::vec3& GetRotation() const { return m_Rotation; }
+        inline const glm::vec3& GetPosition() const { return m_Position; }
+        inline const glm::vec3& GetRotation() const { return m_Rotation; }
 
         inline const glm::mat4& GetViewMatrix()           const { return m_ViewMatrix; }
         inline const glm::mat4& GetViewProjectionMatrix() const { return m_ViewProjectionMatrix; }
