2e133f9b15faebe39d20e0c96edb03d7a21b68a5
ddashdyy <poletaevgena75@gmail.com>
2026-04-10 12:05:26 +0800

    Added index count to Present, new virtual method(GetImageCount and GetRenderPass)

--- Flux/src/Flux/Renderer/RHISwapchain.h
+++ Flux/src/Flux/Renderer/RHISwapchain.h
@@ -3,6 +3,7 @@
 
 #include "RHICommon.h"
 #include "RHISemaphore.h"
+#include "RHIRenderPass.h"
 
 #include <cstdint>
 
@@ -15,10 +16,13 @@ namespace Flux {
         virtual ~RHISwapchain() = default;
 
         virtual uint32_t AcquireNextImage(RHISemaphore* semaphore) = 0;
-        virtual void     Present(RHISemaphore* semaphore) = 0;
+        virtual void     Present(RHISemaphore* semaphore, uint32_t imageIndex) = 0;
         virtual void     Resize(uint32_t w, uint32_t h) = 0;
         virtual uint32_t GetCurrentImageIndex() const = 0;
         virtual Format   GetFormat() const = 0;
+        virtual uint32_t GetImageCount() const = 0;
+
+        virtual RHIRenderPass* GetRenderPass() const = 0;
     };
 
 }
