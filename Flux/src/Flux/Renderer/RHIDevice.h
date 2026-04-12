4d108cad9ef26a20d3f1a0a7a98c31a54ba3d5c1
ddashdyy <poletaevgena75@gmail.com>
2026-04-12 15:18:30 +0800

    Added Creating Framebuffers and Device memory statistics

--- Flux/src/Flux/Renderer/RHIDevice.h
+++ Flux/src/Flux/Renderer/RHIDevice.h
@@ -4,18 +4,30 @@
 #include "Flux/Core.h"
 
 #include "RHIBuffer.h"
-#include "RHITexture.h"
+#include "RHICommandList.h"
+#include "RHIDescriptorSet.h"
+#include "RHIFence.h"
+#include "RHIFramebuffer.h"
 #include "RHIPipeline.h"
 #include "RHIRenderPass.h"
-#include "RHIShader.h"
-#include "RHIFence.h"
 #include "RHISemaphore.h"
-#include "RHICommandList.h"
+#include "RHIShader.h"
 #include "RHISwapchain.h"
-#include "RHIDescriptorSet.h"
-
+#include "RHITexture.h"
 
 namespace Flux {
+
+    struct DeviceMemoryStats
+    {
+        uint32_t AllocationCount;
+        uint64_t AllocationBytes;
+        
+        uint32_t BlockCount;
+        uint64_t BlockBytes;
+
+        uint64_t Usage;
+        uint64_t Budget;
+    };
 
     class RHIDevice 
     {
@@ -24,6 +36,7 @@ namespace Flux {
 
         virtual Scope<RHIBuffer>              CreateBuffer(const BufferSpec& spec) = 0;
         virtual Scope<RHITexture>             CreateTexture(const TextureSpec& spec) = 0;
+        virtual Scope<RHIFramebuffer>         CreateFramebuffer(const FramebufferSpec& spec) = 0;
         virtual Scope<RHIPipeline>            CreatePipeline(const PipelineDesc& desc) = 0;
         virtual Scope<RHIRenderPass>          CreateRenderPass(const RenderPassDesc& desc) = 0;
         virtual Scope<RHIShader>              CreateShader(ShaderStage stage, const std::vector<uint32_t>& spirv) = 0;
@@ -34,6 +47,8 @@ namespace Flux {
 
         virtual RHICommandList* GetCommandList(uint32_t index = 0) = 0;
         virtual RHISwapchain* GetSwapchain() = 0;
+
+        virtual DeviceMemoryStats GetMemoryStatistics() const = 0;
     };
 
 }
