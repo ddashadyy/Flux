8178c22c13a863d470912f1eb525261355341312
ddashdyy <poletaevgena75@gmail.com>
2026-04-13 14:52:02 +0800

    Perspective Camera

--- Flux/src/Flux/Renderer/PerspectiveCamera.cpp
+++ Flux/src/Flux/Renderer/PerspectiveCamera.cpp
@@ -1,55 +1,41 @@
 #include "flpch.h"
 #include "PerspectiveCamera.h"
 #include "Flux/Core.h"
-
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace Flux {
-	PerspectiveCamera::PerspectiveCamera(float fov, float aspectRatio, float nearClip, float farClip)
-		: Camera(glm::perspective(glm::radians(fov), aspectRatio, nearClip, farClip)),
-		m_FOV(fov), m_AspectRatio(aspectRatio), m_NearClip(nearClip), m_FarClip(farClip) 
-	{
-		RecalculateViewMatrix();
-		FL_CORE_INFO("Camera position: {0}, {1}, {2}", GetPosition().x, GetPosition().y, GetPosition().z);
-	}
 
-	void PerspectiveCamera::SetPosition(const glm::vec3& position)
-	{
-		m_Position = position;
-		RecalculateViewMatrix();
-	}
+    PerspectiveCamera::PerspectiveCamera(float fov, float aspectRatio, float nearClip, float farClip)
+        : Camera(glm::perspective(glm::radians(fov), aspectRatio, nearClip, farClip)),
+        m_FOV(fov), m_AspectRatio(aspectRatio), m_NearClip(nearClip), m_FarClip(farClip)
+    {
+        m_Rotation = glm::vec3(0.0f, -90.0f, 0.0f);
+        m_Position = glm::vec3(0.0f, 0.0f, 3.0f);
+        RecalculateViewMatrix();
+    }
 
-	void PerspectiveCamera::SetRotation(const glm::vec3& rotation)
-	{
-		m_Rotation = rotation;
-		RecalculateViewMatrix();
-	}
+    void PerspectiveCamera::SetPosition(const glm::vec3& position)
+    {
+        m_Position = position;
+        RecalculateViewMatrix();
+    }
 
-	void PerspectiveCamera::RecalculateViewMatrix()
-	{
-		glm::vec3 front{};
-		front.x = cos(glm::radians(m_Rotation.y)) * cos(glm::radians(m_Rotation.x));
-		front.y = sin(glm::radians(m_Rotation.x));
-		front.z = sin(glm::radians(m_Rotation.y)) * cos(glm::radians(m_Rotation.x));
+    void PerspectiveCamera::SetRotation(const glm::vec3& rotation)
+    {
+        m_Rotation = rotation;
+        RecalculateViewMatrix();
+    }
 
-		FL_CORE_TRACE("Front coords is {0}, {1}, {2}", front.x, front.y, front.z);
+    void PerspectiveCamera::RecalculateViewMatrix()
+    {
+        glm::vec3 front;
+        front.x = cos(glm::radians(m_Rotation.y)) * cos(glm::radians(m_Rotation.x));
+        front.y = sin(glm::radians(m_Rotation.x));
+        front.z = sin(glm::radians(m_Rotation.y)) * cos(glm::radians(m_Rotation.x));
+        front = glm::normalize(front);
 
-		front = glm::normalize(front);
+        m_ViewMatrix = glm::lookAt(m_Position, m_Position + front, glm::vec3(0.0f, 1.0f, 0.0f));
+        m_ViewProjectionMatrix = GetProjection() * m_ViewMatrix;
+    }
 
-		m_ViewMatrix = glm::lookAt(
-			m_Position,           
-			m_Position + front,   
-			glm::vec3(0.0f, 1.0f, 0.0f)    
-		);
-
-		m_ViewProjectionMatrix = GetProjection() * m_ViewMatrix;
-
-		//m_ViewMatrix = glm::lookAt(
-		//	glm::vec3(0.0f, 0.0f, 2.0f),
-		//	glm::vec3(0.0f, 0.0f, 0.0f),
-		//	glm::vec3(0.0f, 1.0f, 0.0f)
-		//);
-
-		//m_ViewProjectionMatrix = GetProjection() * m_ViewMatrix;
-	}
-}
+} 
--- Flux/src/Flux/Renderer/PerspectiveCamera.h
+++ Flux/src/Flux/Renderer/PerspectiveCamera.h
@@ -17,6 +17,10 @@ namespace Flux {
         inline const glm::vec3& GetPosition() const { return m_Position; }
         inline const glm::vec3& GetRotation() const { return m_Rotation; }
 
+		inline const float GetFOV() const { return m_FOV; }
+		inline void SetFOV(float fov) { m_FOV = fov; }
+		inline void SetAspectRatio(float aspectRatio) { m_AspectRatio = aspectRatio; }
+
         inline const glm::mat4& GetViewMatrix()           const { return m_ViewMatrix; }
         inline const glm::mat4& GetViewProjectionMatrix() const { return m_ViewProjectionMatrix; }
 
