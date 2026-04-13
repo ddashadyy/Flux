b56a83b38c20e4be26073b7a5135b54963971251
ddashdyy <poletaevgena75@gmail.com>
2026-04-13 14:39:58 +0800

    Added OnResize method

--- Flux/src/Flux/Layer.h
+++ Flux/src/Flux/Layer.h
@@ -18,6 +18,7 @@ namespace Flux {
         virtual void OnUpdate(RHICommandList* cmdList) {}
         virtual void OnImGuiRender() {}
         virtual void OnEvent(Event& event) {}
+        virtual void OnResize(uint32_t width, uint32_t height) {}
 
         inline const std::string& GetName() const { return m_DebugName; }
 
