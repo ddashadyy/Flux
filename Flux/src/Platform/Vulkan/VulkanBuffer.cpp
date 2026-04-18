89d346a0b9f1aa52274f712f465d2cff872443b5
ddashdyy <poletaevgena75@gmail.com>
2026-04-18 19:44:59 +0800

    Added string buffer usage

--- Flux/src/Platform/Vulkan/VulkanBuffer.cpp
+++ Flux/src/Platform/Vulkan/VulkanBuffer.cpp
@@ -7,8 +7,8 @@ namespace Flux {
 	{
 		switch (usage)
 		{
-			case BufferUsage::Vertex:  return VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-			case BufferUsage::Index:   return VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
+			case BufferUsage::Vertex:  return VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+			case BufferUsage::Index:   return VK_BUFFER_USAGE_INDEX_BUFFER_BIT  | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
 			case BufferUsage::Uniform: return VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
 			case BufferUsage::Storage: return VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
 			case BufferUsage::Staging: return VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
@@ -16,6 +16,21 @@ namespace Flux {
 
 		FL_CORE_ASSERT(false, "Unknown buffer usage!");
 		return VK_BUFFER_USAGE_FLAG_BITS_MAX_ENUM;
+	}
+
+	static const char* BufferUsageToString(BufferUsage usage)
+	{
+		switch (usage)
+		{
+		case BufferUsage::Vertex:  return "Vertex";
+		case BufferUsage::Index:   return "Index";
+		case BufferUsage::Uniform: return "Uniform";
+		case BufferUsage::Storage: return "Storage";
+		case BufferUsage::Staging: return "Staging";
+		}
+
+		FL_CORE_ASSERT(false, "Unknown buffer usage!");
+		return "Unknown";
 	}
 
 	VulkanBuffer::VulkanBuffer(VmaAllocator allocator, const BufferSpec& spec)
@@ -31,7 +46,7 @@ namespace Flux {
 
 		vmaCreateBuffer(m_Allocator, &bufferInfo, &allocInfo, &m_Buffer, &m_Allocation, nullptr);
 
-		FL_CORE_INFO("Created Vulkan buffer, size {0} bytes", spec.Size);
+		FL_CORE_INFO("Created Vulkan buffer, size {0} bytes, usage: {1}", spec.Size, BufferUsageToString(spec.Usage));
 	}
 
 	VulkanBuffer::~VulkanBuffer()
