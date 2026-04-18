46863e234272a24b90a624b93bf1c397e35cbcde
ddashdyy <poletaevgena75@gmail.com>
2026-04-18 19:41:41 +0800

    Added SampleCount to TextureSpec

--- Flux/src/Flux/Renderer/RHITexture.h
+++ Flux/src/Flux/Renderer/RHITexture.h
@@ -9,15 +9,16 @@ namespace Flux {
     {
         Texture2D = 0,
         Texture3D = 1,
-        CubeMap = 2
+        CubeMap = 2,
     };
 
     enum class TextureUsage : uint8_t
     {
-        Sampled      = 0, // читается в шейдере
-        RenderTarget = 1, // color attachment
-        DepthStencil = 2, // depth attachment
-        Storage      = 3, // read/write в compute
+        Sampled      = 0, 
+        RenderTarget = 1, 
+        DepthStencil = 2, 
+        Storage      = 3, 
+        Transient    = 4
     };
 
     struct TextureSpec 
@@ -28,6 +29,8 @@ namespace Flux {
 
         Format       ImageFormat = Format::R8G8B8A8_UNORM;
         uint32_t     MipLevels   = 1;
+
+        SampleCount  Samples     = SampleCount::x1;
 
         TextureUsage Usage       = TextureUsage::Sampled;
         TextureType  Type        = TextureType::Texture2D;
@@ -42,6 +45,8 @@ namespace Flux {
 		virtual uint32_t GetHeight()    const = 0;
 		virtual Format   GetFormat()    const = 0;
         virtual uint32_t GetMipLevels() const = 0;
+
+        virtual void* GetNativeImageView() const = 0;
 
         virtual void SetData(const void* data, uint32_t size) = 0;
         
