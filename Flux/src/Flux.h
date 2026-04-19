c8f2e365e2044d1b48b5e0b3affe2e1db8827ad0
ddashdyy <poletaevgena75@gmail.com>
2026-04-19 19:22:27 +0800

    Flux header file

--- Flux/src/Flux.h
+++ Flux/src/Flux.h
@@ -2,14 +2,9 @@
 
 
 // For use Flux applications
-
 #include "Flux/Core/Application.h"
 #include "Flux/Core/Layer.h"
 #include "Flux/Core/Log.h"
-
-#include "Flux/Core/Input.h"
-#include "Flux/Core/KeyCodes.h"
-#include "Flux/Core/MouseButtonCodes.h"
 
 #include "Flux/ImGui/ImGuiLayer.h"
 
@@ -18,29 +13,24 @@
 // ----------------------
 
 // ---Input-------------
-#include "Platform/Windows/WindowsInput.h"
+#include "Flux/Core/Input.h"
+#include "Flux/Core/KeyCodes.h"
+#include "Flux/Core/MouseButtonCodes.h"
 
 // ---Geometry-------------
 #include "Flux/Renderer/Geometry.h"
 // ------------------------
 
 // ---Renderer-------------
-#include "Flux/Renderer/PerspectiveCamera.h"
-
 #include "Flux/Renderer/RHIBuffer.h"
 #include "Flux/Renderer/RHIPipeline.h"
 #include "Flux/Renderer/RHIShader.h"
 #include "Flux/Renderer/RHITexture.h"
 #include "Flux/Renderer/RHIFence.h"
 #include "Flux/Renderer/RHISemaphore.h"
-
 #include "Flux/Renderer/RendererBackend.h"
-
-
-
 // ------------------------
 
 // ---Entry Point-----------
 #include "Flux/Core/EntryPoint.h"
-
 // -------------------------
