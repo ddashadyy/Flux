34dc84c6fbec5b7617f705e2550c184e414ca0c1
ddashdyy <poletaevgena75@gmail.com>
2026-05-04 13:48:25 +0800

    File Dialog

---
+++ Flux/vendor/imguifiledialog/premake5.lua
@@ -0,0 +1,26 @@
+project "ImGuiFileDialog"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files { "include/ImGuiFileDialog.cpp" }
+
+    includedirs {
+        "include",
+        "%{wks.location}/Flux/vendor/imgui"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
