9b3d66224bb730e813fe61414c84100beda9441a
ddashdyy <poletaevgena75@gmail.com>
2026-05-04 13:37:58 +0800

    Vulkan Buffer impl

--- Flux/src/Platform/Vulkan/VulkanBuffer.cpp
+++ Flux/src/Platform/Vulkan/VulkanBuffer.cpp
@@ -3,67 +3,59 @@
 
 namespace Flux {
 
-	static VkBufferUsageFlags GetVkBufferUsageFlags(BufferUsage usage)
-	{
-		switch (usage)
-		{
-			case BufferUsage::Vertex:  return VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
-			case BufferUsage::Index:   return VK_BUFFER_USAGE_INDEX_BUFFER_BIT  | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
-			case BufferUsage::Uniform: return VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
-			case BufferUsage::Storage: return VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
-			case BufferUsage::Staging: return VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-		}
+    static VkBufferUsageFlags GetVkBufferUsageFlags(BufferUsage usage)
+    {
+        VkBufferUsageFlags flags = 0;
 
-		FL_CORE_ASSERT(false, "Unknown buffer usage!");
-		return VK_BUFFER_USAGE_FLAG_BITS_MAX_ENUM;
-	}
+        if (static_cast<uint8_t>(usage & BufferUsage::Vertex))
+            flags |= VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+        if (static_cast<uint8_t>(usage & BufferUsage::Index))
+            flags |= VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+        if (static_cast<uint8_t>(usage & BufferUsage::Uniform))
+            flags |= VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+        if (static_cast<uint8_t>(usage & BufferUsage::Storage))
+            flags |= VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+        if (static_cast<uint8_t>(usage & BufferUsage::Staging))
+            flags |= VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+        if (static_cast<uint8_t>(usage & BufferUsage::Indirect))
+            flags |= VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT;
 
-	static const char* BufferUsageToString(BufferUsage usage)
-	{
-		switch (usage)
-		{
-		case BufferUsage::Vertex:  return "Vertex";
-		case BufferUsage::Index:   return "Index";
-		case BufferUsage::Uniform: return "Uniform";
-		case BufferUsage::Storage: return "Storage";
-		case BufferUsage::Staging: return "Staging";
-		}
+        FL_CORE_ASSERT(flags != 0, "BufferUsage resulted in empty VkBufferUsageFlags!");
+        return flags;
+    }
 
-		FL_CORE_ASSERT(false, "Unknown buffer usage!");
-		return "Unknown";
-	}
+    VulkanBuffer::VulkanBuffer(VmaAllocator allocator, const BufferSpec& spec)
+        : m_Spec(spec), m_Allocator(allocator)
+    {
+        VkBufferCreateInfo bufferInfo{ VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO };
+        bufferInfo.size = spec.Size;
+        bufferInfo.usage = GetVkBufferUsageFlags(spec.Usage);
 
-	VulkanBuffer::VulkanBuffer(VmaAllocator allocator, const BufferSpec& spec)
-		: m_Spec(spec), m_Allocator(allocator)
-	{
-		VkBufferCreateInfo bufferInfo{};
-		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-		bufferInfo.size = spec.Size;
-		bufferInfo.usage = GetVkBufferUsageFlags(spec.Usage);
-		
-		VmaAllocationCreateInfo allocInfo{};
-		allocInfo.usage = spec.CpuVisible ? VMA_MEMORY_USAGE_CPU_TO_GPU : VMA_MEMORY_USAGE_GPU_ONLY;
+        VmaAllocationCreateInfo allocInfo{};
+        allocInfo.usage = spec.CpuVisible ? VMA_MEMORY_USAGE_CPU_TO_GPU : VMA_MEMORY_USAGE_GPU_ONLY;
 
-		vmaCreateBuffer(m_Allocator, &bufferInfo, &allocInfo, &m_Buffer, &m_Allocation, nullptr);
+        FL_CORE_ASSERT(vmaCreateBuffer(m_Allocator, &bufferInfo, &allocInfo,
+            &m_Buffer, &m_Allocation, nullptr) == VK_SUCCESS,
+            "Failed to create Vulkan Buffer");
 
-		FL_CORE_INFO("Created Vulkan buffer, size {0} bytes, usage: {1}", spec.Size, BufferUsageToString(spec.Usage));
-	}
+        FL_CORE_INFO("Created Vulkan Buffer {} bytes", spec.Size);
+    }
 
-	VulkanBuffer::~VulkanBuffer()
-	{
-		vmaDestroyBuffer(m_Allocator, m_Buffer, m_Allocation);
-	}
+    VulkanBuffer::~VulkanBuffer()
+    {
+        vmaDestroyBuffer(m_Allocator, m_Buffer, m_Allocation);
+    }
 
-	void* VulkanBuffer::Map()
-	{
-		void* data;
-		vmaMapMemory(m_Allocator, m_Allocation, &data);
+    void* VulkanBuffer::Map()
+    {
+        void* data;
+        vmaMapMemory(m_Allocator, m_Allocation, &data);
+        return data;
+    }
 
-		return data;
-	}
+    void VulkanBuffer::Unmap()
+    {
+        vmaUnmapMemory(m_Allocator, m_Allocation);
+    }
 
-	void VulkanBuffer::Unmap()
-	{
-		vmaUnmapMemory(m_Allocator, m_Allocation);
-	}
-}
+} // namespace Flux
--- Flux/src/Platform/Vulkan/VulkanBuffer.h
+++ Flux/src/Platform/Vulkan/VulkanBuffer.h
@@ -1,34 +1,31 @@
 #pragma once
 
-
 #include "Flux/Renderer/RHIBuffer.h"
 
-#include "vulkan/vulkan.h"
-#include "vk_mem_alloc.h"
+#include <vulkan/vulkan.h>
+#include <vk_mem_alloc.h>
 
 namespace Flux {
 
-	class VulkanBuffer final : public RHIBuffer
-	{
-	public:
-		VulkanBuffer(VmaAllocator allocator, const BufferSpec& spec);
-		~VulkanBuffer();
+    class VulkanBuffer final : public RHIBuffer
+    {
+    public:
+        VulkanBuffer(VmaAllocator allocator, const BufferSpec& spec);
+        ~VulkanBuffer();
 
-		void* Map() override;
-		void  Unmap() override;
+        void* Map()   override;
+        void  Unmap() override;
 
-		uint64_t    GetSize()   const override { return m_Spec.Size; }
-		BufferUsage GetUsage()  const override { return m_Spec.Usage; }
+        const BufferSpec& GetSpec() const override { return m_Spec; }
 
-	private:
-		BufferSpec m_Spec{};
-		VkBuffer   m_Buffer = VK_NULL_HANDLE;
+    private:
+        BufferSpec    m_Spec{};
+        VkBuffer      m_Buffer = VK_NULL_HANDLE;
+        VmaAllocator  m_Allocator = VK_NULL_HANDLE;
+        VmaAllocation m_Allocation = VK_NULL_HANDLE;
 
-		VmaAllocator  m_Allocator  = VK_NULL_HANDLE;
-		VmaAllocation m_Allocation = VK_NULL_HANDLE;
+    protected:
+        void* GetHandleImpl() const override { return m_Buffer; }
+    };
 
-	protected:
-		void* GetHandleImpl() const override { return m_Buffer; }
-	};
-
-}
+} // namespace Flux
