1a9a40a1195e55e573a945bf62f51d8b8a094268
ddashdyy <poletaevgena75@gmail.com>
2026-05-14 19:52:54 +0800

    Now handles is not template methods

--- Flux/src/Platform/Vulkan/VulkanFramebuffer.cpp
+++ Flux/src/Platform/Vulkan/VulkanFramebuffer.cpp
@@ -23,7 +23,7 @@ namespace Flux {
 
         VkFramebufferCreateInfo fbInfo{};
         fbInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
-        fbInfo.renderPass = vkPass->GetHandle<VkRenderPass>();
+        fbInfo.renderPass = static_cast<VkRenderPass>(vkPass->GetHandle());
         fbInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
         fbInfo.pAttachments = attachments.data();
         fbInfo.width = m_Spec.Width;
