61d51799add98bbc917ba0bcfce530a3aab98d21
ddashdyy <poletaevgena75@gmail.com>
2026-05-14 19:41:41 +0800

    Added small component system

--- Flux/src/Flux/Scene/Entity.h
+++ Flux/src/Flux/Scene/Entity.h
@@ -5,6 +5,9 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/quaternion.hpp>
 
+#include <optional>
+#include <string>
+
 #include "Flux/Renderer/RHICommandList.h"
 #include "Flux/Renderer/AssetManager.h" 
 
@@ -25,6 +28,14 @@ namespace Flux {
         }
     };
 
+    struct LightComponent
+    {
+        enum class LightType { Point, Directional, Spot } Type = LightType::Point;
+        glm::vec3 Color = { 1.0f, 1.0f, 1.0f };
+        float Intensity = 1.0f;
+        float Range = 10.0f;
+    };
+
     class Entity
     {
     public:
@@ -40,9 +51,26 @@ namespace Flux {
         const std::string& GetName() const { return m_Name; }
         void               SetName(const std::string& name) { m_Name = name; }
 
+
+        void MarkForDeletion()           { m_MarkedForDeletion = true; }
+        bool IsMarkedForDeletion() const { return m_MarkedForDeletion; }
+
+        // --- МЕТОДЫ ДЛЯ РАБОТЫ С КОМПОНЕНТАМИ ---
+        bool                  HasLight() const { return m_Light.has_value(); }
+
+        LightComponent&       GetLight() { return m_Light.value(); }
+        const LightComponent& GetLight() const { return m_Light.value(); }
+
+        void AddLight()    { m_Light.emplace(); }
+        void RemoveLight() { m_Light.reset(); }
+
     private:
         std::string m_Name;
         Transform   m_Transform{};
         Ref<Model>  m_Model;
+
+        std::optional<LightComponent> m_Light;
+
+        bool m_MarkedForDeletion = false;
     };
 }
