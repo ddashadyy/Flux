420d40b8d86f711a18d69bc22741faa39275e953
ddashdyy <poletaevgena75@gmail.com>
2026-05-23 20:53:59 +0800

    Animations structs

--- Flux/src/Flux/Renderer/Geometry.h
+++ Flux/src/Flux/Renderer/Geometry.h
@@ -2,6 +2,8 @@
 
 
 #include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
 
 #include "RHIBuffer.h"
 #include "RHIDevice.h"
@@ -36,6 +38,71 @@ namespace Flux {
                 TexCoord == other.TexCoord &&
                 Tangent == other.Tangent;  
         }
+    };
+
+    struct SkinnedVertex
+    {
+        glm::vec3  Position;
+        glm::vec3  Normal;
+        glm::vec2  TexCoord;
+        glm::vec3  Tangent;
+        glm::uvec4 JointIndices{ 0, 0, 0, 0 };
+        glm::vec4  JointWeights{ 1, 0, 0, 0 };
+    };
+
+    struct Joint
+    {
+        std::string Name;
+        int32_t     ParentIndex = -1;      // -1 = root
+        glm::mat4   InverseBindMatrix{ 1.0f };
+    };
+
+    struct Skeleton
+    {
+        std::vector<Joint> Joints;         
+
+        int FindJoint(const std::string& name) const
+        {
+            for (int i = 0; i < (int)Joints.size(); i++)
+                if (Joints[i].Name == name) return i;
+            return -1;
+        }
+    };
+
+    template<typename T>
+    struct AnimationChannel
+    {
+        std::vector<float> Times;
+        std::vector<T>     Values;
+
+        T Sample(float t) const
+        {
+            if (Times.empty()) return T{};
+            if (t <= Times.front()) return Values.front();
+            if (t >= Times.back())  return Values.back();
+
+            auto it = std::upper_bound(Times.begin(), Times.end(), t);
+            int hi = (int)std::distance(Times.begin(), it);
+            int lo = hi - 1;
+
+            float alpha = (t - Times[lo]) / (Times[hi] - Times[lo]);
+
+            if constexpr (std::is_same_v<T, glm::quat>)
+                return glm::normalize(glm::slerp(Values[lo], Values[hi], alpha));
+            else
+                return glm::mix(Values[lo], Values[hi], alpha);
+        }
+    };
+
+    struct AnimationClip
+    {
+        std::string Name;
+        float       Duration = 0.f;
+
+        // per-joint каналы
+        std::vector<AnimationChannel<glm::vec3>> PositionChannels;
+        std::vector<AnimationChannel<glm::quat>> RotationChannels;
+        std::vector<AnimationChannel<glm::vec3>> ScaleChannels;
     };
 
     using Texture = RHITexture;
@@ -75,6 +142,12 @@ namespace Flux {
         Scope<RHIBuffer>     VertexBuffer;
         std::vector<SubMesh> Meshes;
         AABB                 Bounds;
+
+        // Skinning
+        bool                           IsSkinned = false;
+        Ref<Skeleton>                  Skel;
+        std::vector<SkinnedVertex>     SkinnedVertices;
+        std::vector<Ref<AnimationClip>> Animations;
 
         const std::filesystem::path& GetPath() const { return Path; }
     };
